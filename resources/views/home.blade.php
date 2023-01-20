a5ab50dd61eca03805f7c527f1cf7344d5fef8b5
FrancescoGentile97 <francesco.gentile97@icloud.com>
2023-01-20 06:37:17 +0100

    aggiunto html e bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,23 @@
-<h1>Hello World</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+
+    <title>Home Laravel</title>
+</head>
+<body>
+    <div class="container p-2">
+        <div class="text-center">
+            <h1>Benvenuto nella Home</h1>
+        </div>
+        <div class="d-flex justify-content-between">
+            <a href="">Info</a>
+            <a href="">Contatti</a>
+        </div>
+    </div>
+    
+</body>
+</html>
